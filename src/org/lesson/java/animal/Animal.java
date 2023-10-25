6d7f17da8e043ebdc434c58c3a3e32e4bfdafc0d
DanieleCorrado <corrado_daniele@virgilio.it>
2023-10-25 14:18:53 +0200

    Initial commit

---
+++ src/org/lesson/java/animal/Animal.java
@@ -0,0 +1,24 @@
+package org.lesson.java;
+
+public class Animal {
+
+    // Atributi
+
+    String name;
+
+    // Costruttori
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    // Metodi
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
